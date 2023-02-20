f71ff362d885911eb75975641788b9470b3052ab
Nanzinha <ana.flaviap7@hotmail.com>
2023-02-20 17:51:48 -0300

    feat: finish crud

--- 1 - Applications/CRUD with Database MySQL/main.go
+++ 1 - Applications/CRUD with Database MySQL/main.go
@@ -12,6 +12,10 @@ func main() {
 	router := mux.NewRouter()
 	/*Chamando a função de criar usuário que está no pacote server*/
 	router.HandleFunc("/users", server.CreateUser).Methods(http.MethodPost)
+	router.HandleFunc("/users", server.GetAllUsers).Methods(http.MethodGet)
+	router.HandleFunc("/users/{id}", server.GetUser).Methods(http.MethodGet)
+	router.HandleFunc("/users/{id}", server.UpdateUser).Methods(http.MethodPut)
+	router.HandleFunc("/users/{id}", server.DeleteUser).Methods(http.MethodDelete)
 
 	fmt.Println("Listening on 5000 Port")
 	log.Fatal(http.ListenAndServe(":5000", router))
--- 1 - Applications/CRUD with Database MySQL/server/server.go
+++ 1 - Applications/CRUD with Database MySQL/server/server.go
@@ -1,11 +1,14 @@
 package server
 
 import (
-	"fmt"
 	"crud/database"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type user struct {
@@ -14,6 +17,7 @@ type user struct {
 	Email string `json:"email"`
 }
 
+//Create a new user on database, expected name and email
 func CreateUser(w http.ResponseWriter, r *http.Request) {
 	bodyRequest, error := ioutil.ReadAll(r.Body)
 	if error != nil {
@@ -24,6 +28,7 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	var user user
 	if error = json.Unmarshal(bodyRequest, &user); error != nil {
 		w.Write([]byte("Error to transform user to struct!"))
+		return
 	}
 
 	db, error := database.Connect()
@@ -57,3 +62,170 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte(fmt.Sprintf("User inserted sucessfully! ID: %d", idInserted)))
 }
+
+//Get all users from database
+func GetAllUsers(w http.ResponseWriter, r *http.Request) {
+	db, error := database.Connect()
+	if error != nil {
+		w.Write([]byte("Error to connect on database!"))
+		return
+	}
+	defer db.Close()
+
+	//Prepare query
+	rows, error := db.Query("select * from users")
+	if error != nil {
+		w.Write([]byte("Error to search users from database!"))
+		return
+	}
+	defer rows.Close()
+
+	//Slice de usuários
+	var users []user
+
+	//Lendo um por um
+	for rows.Next() {
+		var user user
+
+		if error := rows.Scan(&user.ID, &user.Name, &user.Email); error != nil {
+			w.Write([]byte("Error to scan user"))
+			return
+		}
+
+		users = append(users, user)
+	}
+
+	//Status Code
+	w.WriteHeader(http.StatusOK)
+	if error := json.NewEncoder(w).Encode(users); error != nil {
+		w.Write([]byte("Error to transform slice users in JSON"))
+		return 
+	}
+}
+
+//Get an user from database, expected ID for search
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	param := mux.Vars(r)
+
+	//Nome do parametro, base numérica (decimal) e tipo int32 que foi declarado no struct
+	ID, error := strconv.ParseUint(param["id"], 10, 32)
+	if error != nil {
+		w.Write([]byte("Error to transform the request param to integer"))
+		return 
+	}
+
+	db, error := database.Connect()
+	if error != nil {
+		w.Write([]byte("Error to connect on database!"))
+		return
+	}
+	defer db.Close()
+
+	//Prepare query
+	row, error := db.Query("select * from users where id = ?", ID)
+	if error != nil {
+		w.Write([]byte("Error to search the user from database!"))
+		return
+	}
+
+	var user user
+	if row.Next() {
+		if error := row.Scan(&user.ID, &user.Name, &user.Email); error != nil {
+			w.Write([]byte("Error to scan user"))
+			return
+		}
+	}
+
+	//Status Code
+	w.WriteHeader(http.StatusOK)
+	if error := json.NewEncoder(w).Encode(user); error != nil {
+		w.Write([]byte("Error to transform user in JSON"))
+		return 
+	}
+}
+
+//Update user data on database
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	param := mux.Vars(r)
+
+	//Nome do parametro, base numérica (decimal) e tipo int32 que foi declarado no struct
+	ID, error := strconv.ParseUint(param["id"], 10, 32)
+	if error != nil {
+		w.Write([]byte("Error to transform the request param to integer"))
+		return 
+	}
+
+	bodyRequest, error := ioutil.ReadAll(r.Body)
+	if error != nil {
+		w.Write([]byte("Fail to read body request!"))
+		return
+	}
+
+	var user user
+	if error = json.Unmarshal(bodyRequest, &user); error != nil {
+		w.Write([]byte("Error to transform user to struct!"))
+		return
+	}
+
+	db, error := database.Connect()
+	if error != nil {
+		w.Write([]byte("Error to connect on database!"))
+		return
+	}
+	defer db.Close()
+
+	//Prepare Statment
+	statement, error := db.Prepare("update users set name = ?, email = ? where id = ?")
+	if error != nil {
+		w.Write([]byte("Error to create the statement!"))
+		return
+	}
+	defer statement.Close()
+
+	if _, error := statement.Exec(user.Name, user.Email, ID); error != nil {
+		w.Write([]byte("Error to update the user!"))
+		return
+	}
+
+	//Status Code
+	w.WriteHeader(http.StatusNoContent)
+	w.Write([]byte(fmt.Sprintf("User updated sucessfully! ID: %d", ID)))
+}
+
+//Delete user data from database
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	param := mux.Vars(r)
+
+	//Nome do parametro, base numérica (decimal) e tipo int32 que foi declarado no struct
+	ID, error := strconv.ParseUint(param["id"], 10, 32)
+	if error != nil {
+		w.Write([]byte("Error to transform the request param to integer"))
+		return 
+	}
+
+	db, error := database.Connect()
+	if error != nil {
+		w.Write([]byte("Error to connect on database!"))
+		return
+	}
+	defer db.Close()
+
+	//Prepare Statment
+	statement, error := db.Prepare("delete from users where id = ?")
+	if error != nil {
+		w.Write([]byte("Error to create the statement!"))
+		return
+	}
+	defer statement.Close()
+
+	if _, error := statement.Exec(ID); error != nil {
+		w.Write([]byte("Error to delete the user!"))
+		return
+	}
+
+	//Status Code
+	w.WriteHeader(http.StatusNoContent)
+	w.Write([]byte(fmt.Sprintf("User deleted sucessfully! ID: %d", ID)))
+}
+
+
